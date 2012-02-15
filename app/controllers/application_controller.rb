fdc72d0bb1a03c32f8e4bd143178f5f0a22582aa
yammerganesh <gkumaraswamy@yammer-inc.com>
2012-02-14 21:41:21 -0800

    HTTPS redirect fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,10 @@ class ApplicationController < ActionController::Base
   APP_DOMAIN = 'www.yammerfall.com'
 
   def ensure_domain
-    if (request.env['HTTP_HOST']!=APP_DOMAIN || request.env['HTTPS']!='on')
-      # HTTP 301 is a "permanent" redirect
+    if request.env['HTTP_HOST'] != APP_DOMAIN
       redirect_to "https://#{APP_DOMAIN}", :status => 301
+    elsif request.env['HTTPS'] != 'on'
+   	  redirect_to "https://#{APP_DOMAIN}", :status => 301
     end
   end
 end
