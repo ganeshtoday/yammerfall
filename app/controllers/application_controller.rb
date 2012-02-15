4c639b8ea9620c56a8a21b98592b448d348efc4d
yammerganesh <gkumaraswamy@yammer-inc.com>
2012-02-14 21:33:52 -0800

    Revert "Alternate method for HTTPS redirect"
    This reverts commit b32c1989dbfa2a063b1b2d453b6a792d3c27ff8d.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :ensure_domain
+
+  APP_DOMAIN = 'www.yammerfall.com'
+
+  def ensure_domain
+    if request.env['HTTP_HOST'] != APP_DOMAIN
+      # HTTP 301 is a "permanent" redirect
+      redirect_to "https://#{APP_DOMAIN}", :status => 301
+    end
+  end
 end
