f6d1a0da9513f0fe922e12e31daeff918d3cc573
Ganesh Kumaraswamy <gkumaraswamy@yammer-inc.com>
2012-02-28 11:34:14 -0800

    removed SSL redirects

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  include ::SslRequirement
+  #include ::SslRequirement
 
-  ssl_exceptions
+  #ssl_exceptions
 
-  before_filter :ensure_domain
+  #before_filter :ensure_domain
 
-  APP_DOMAIN = 'www.yammerfall.com'
+  #APP_DOMAIN = 'www.yammerfall.com'
 
-  def ensure_domain
-    if request.env['HTTP_HOST'] != APP_DOMAIN
-      # HTTP 301 is a "permanent" redirect
-      redirect_to "https://#{APP_DOMAIN}", :status => 301
-    end
-  end
+  #def ensure_domain
+  #  if request.env['HTTP_HOST'] != APP_DOMAIN
+  #    # HTTP 301 is a "permanent" redirect
+  #    redirect_to "https://#{APP_DOMAIN}", :status => 301
+  #  end
+  #end
 end
